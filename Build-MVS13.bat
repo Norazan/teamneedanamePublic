4c745ad996850fef65eac2b018acf78c05a948c5
Jordan Ramirez <jordan.ramirez@student.hu.nl>
2015-09-28 15:13:00 +0200

    Correct Build-MVS13.bat

--- Build-MVS13.bat
+++ Build-MVS13.bat
@@ -2,4 +2,8 @@ RMDIR .\Build /s /q
 
 mkdir .\Build
 
-\Windows-cmake\Cmake\bin\cmake.exe .\Windows-cmake\Cmake\bin\cmake .\Gate2158 -G "Visual Studio 12 2013"
+cd .\Build
+ 
+..\Windows-cmake\Cmake\bin\cmake.exe ..\Windows-cmake\Cmake\bin\cmake ..\Gate2158 -G "Visual Studio 12 2013"
+
+PAUSE
