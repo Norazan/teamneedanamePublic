428a3c1740d064bb56896bf273192f0190b9edba
Jordan Ramirez <jordan.ramirez@student.hu.nl>
2015-09-28 14:06:04 +0200

    Added Build folder to git.ignore, Made classes for the WindowsManager

---
+++ Gate2158/Gate2158/WindowManager.h
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------
+//
+// File WindowManager.h
+// Original Auhtor: Jordan Ramirez
+//
+// Description:
+// Manages the window object that is displayed to the user.
+//
+// Editors:
+// 
+// Version: V1.0
+// 
+// 
+//------------------------------------------------------------------
+
+
