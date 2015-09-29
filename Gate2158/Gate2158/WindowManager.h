1f78a0119cf62599c24f4351879ad070cbd839f3
OleAgterberg <oleagterberg@gmail.com>
2015-09-29 13:38:19 +0200

    Cmake new files

---
+++ Gate2158/Gate2158/WindowManager.h
@@ -0,0 +1,30 @@
+// -----------------------------------------------------------------
+//
+// File WindowManager.h
+// Original Auhtor: Jordan Ramirez
+//
+// Description:
+// Manages the window object that is displayed to the user.
+//
+// Editors:
+// 
+//
+// Version: V1.0
+//------------------------------------------------------------------
+
+#ifndef WINDOWMANAGER_H
+#define WINDOWMANAGER_H
+
+#include <SFML\Graphics.hpp>
+
+class WindowManager{
+
+public:
+	WindowManager();
+
+private:
+	sf::RenderWindow window;
+};
+
+
+#endif
