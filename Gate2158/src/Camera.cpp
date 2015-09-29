28e200ea5bba88ebe984665a633710ff0a0f9a8f
Martijn van Dijk <martijn@martijnvandijk.net>
2015-09-29 15:51:58 +0200

    added camera class

---
+++ Gate2158/src/Camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by martijn on 9/29/15.
+//
+
+#include "Camera.hpp"
---
+++ Gate2158/src/Camera.hpp
@@ -0,0 +1,14 @@
+//
+// Created by martijn on 9/29/15.
+//
+
+#ifndef GATE2158_CAMERA_HPP
+#define GATE2158_CAMERA_HPP
+
+
+class Camera {
+
+};
+
+
+#endif //GATE2158_CAMERA_HPP
