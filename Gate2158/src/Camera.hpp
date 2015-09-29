55ec455661a4fd7317769561d1af2fc39083b1f9
OleAgterberg <oleagterberg@gmail.com>
2015-09-29 17:12:17 +0200

    add updateMapObjects() and the draw funtion

--- Gate2158/src/Camera.hpp
+++ Gate2158/src/Camera.hpp
@@ -6,14 +6,17 @@
 #define GATE2158_CAMERA_HPP
 
 #include <SFML/Graphics.hpp>
+#include "MapObject.h"
 
 
 class Camera {
 public:
 	Camera(sf::RenderWindow &window);
-
+	void updateMapObjects(std::vector<MapObject> &mapObjects);
+	void draw();
 private:
 	sf::RenderWindow &window;
+	std::vector<MapObject> &mapObjects{};
 };
 
 
