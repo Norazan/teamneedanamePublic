0b93f4f620f95947b9da3f40d082dc368b415bad
OleAgterberg <oleagterberg@gmail.com>
2015-10-20 19:03:14 +0200

    changed some comments in the camera class

--- Gate2158/src/Camera.hpp
+++ Gate2158/src/Camera.hpp
@@ -12,8 +12,17 @@
 
 class Camera {
 public:
+	//! Constructor of Camera
+	//
+	//! @parameter window
+	//! @parameter currenMap
 	Camera(sf::RenderWindow &window, Map &currentMap);
+
+	//! Draw function
+	//
+	//! Drawed all the MapObjects 
 	void draw();
+
 private:
 	sf::RenderWindow &window;
 	Map &currentMap;
