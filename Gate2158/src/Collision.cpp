57d1777f4cc27a158fd5719acab03b95da95acd9
OleAgterberg <oleagterberg@gmail.com>
2015-10-26 22:46:53 +0100

    Begin of collision

--- Gate2158/src/Collision.cpp
+++ Gate2158/src/Collision.cpp
@@ -3,3 +3,85 @@
 //
 
 #include "Collision.h"
+
+int Collision::checkCollision(MapObject & obj1, MapObject & obj2){
+	std::vector<line> lines_obj1 = getLines(obj1);
+	std::vector<line> lines_obj2 = getLines(obj1);
+	std::vector<line> axes_obj1 = getAxes(lines_obj1);
+	std::vector<line> axes_obj2 = getAxes(lines_obj2);
+	// loop over the axes1
+	int smallest_overlap = 500, intersecting_axes, count = 0;
+	for (auto & axes : axes_obj1) {
+		// project both shapes onto the axis
+		line projection_obj1 = getProjection(axes, lines_obj1);
+		line projection_obj2 = getProjection(axes, lines_obj2);
+		// do the projections overlap?
+		int current_overlap = overlap(projection_obj1, projection_obj2);
+		if (current_overlap = 0) {
+			// then we can guarantee that the shapes do not overlap
+			return 0;
+		}
+		else if(current_overlap < smallest_overlap){
+			smallest_overlap = current_overlap;
+			intersecting_axes = count;
+		}
+		++count;
+	}
+	// loop over the axes2
+	count = 0;
+	for (auto & axes : axes_obj2) {
+		// project both shapes onto the axis
+		line projection_obj1 = getProjection(axes, lines_obj1);
+		line projection_obj2 = getProjection(axes, lines_obj2);
+		// do the projections overlap?
+		int current_overlap = overlap(projection_obj1, projection_obj2);
+		if (current_overlap = 0) {
+			// then we can guarantee that the shapes do not overlap
+			return 0;
+		}
+		else if (current_overlap < smallest_overlap){
+			smallest_overlap = current_overlap;
+			intersecting_axes = count;
+		}
+		++count;
+	}
+	// do stuff with it..
+	return 1;
+}
+
+std::vector<Collision::line> Collision::getLines(MapObject & mo){
+	// something with the convex.
+}
+
+std::vector<Collision::line> Collision::getAxes(std::vector<line> lines){
+	std::vector<line> axes;
+	for (auto & l : lines){
+		// axes is perpendicular to the line, simples way is to do ( -y, x ).
+		line newAxes = { 
+			sf::Vector2f{ -l.position_1.y, l.position_1.x },
+			sf::Vector2f{ -l.position_2.y, l.position_2.x } 
+		};
+		axes.push_back(newAxes);
+	}
+}
+
+Collision::line Collision::getProjection(line axes, std::vector<line> lines){
+	double min = axis.dot(shape.vertices[0]);
+	double max = min;
+	for (int i = 1; i < shape.vertices.length; i++) {
+		// NOTE: the axis must be normalized to get accurate projections
+		double p = axis.dot(shape.vertices[i]);
+		if (p < min) {
+			min = p;
+		}
+		else if (p > max) {
+			max = p;
+		}
+	}
+	Projection proj = new Projection(min, max);
+	return proj;
+}
+
+int Collision::overlap(line projection_1, line projection_2){
+
+}
--- Gate2158/src/Collision.h
+++ Gate2158/src/Collision.h
@@ -5,11 +5,41 @@
 #ifndef GATE2158_COLLISION_H
 #define GATE2158_COLLISION_H
 
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "MapObject.h"
+
+
 class Collision {
 public:
+	// returns 1 if collision is detected otherwise it returns 0;
+	int checkCollision(MapObject & ob1, MapObject & ob2);
+
+	// 1. take every line of the two shapes
+	// 2. make axis from the lines, by normalize the line.
+	// 3. make for every axis a projection of the shapes.
+	// 4. if on every axis the projections overlap, the shapes are intersecting.
+	// 5. the axis where the smallest overlap is, is the line where the shapes are intersecting.
+
 
 private:
+	struct line {
+		sf::Vector2f position_1;
+		sf::Vector2f position_2;
+	};
 
+	// returns a vector with all the lines of the object.
+	std::vector<line> getLines(MapObject & mo);
+
+	// returns a vector with all the axes of the lines of an object.
+	std::vector<line> getAxes(std::vector<line> lines);
+
+	// returns the projection
+	line getProjection(line axes, std::vector<line> lines );
+
+	// returns the overlap
+	int overlap(line projection_1, line projection_2);
+	
 };
 
 #endif
