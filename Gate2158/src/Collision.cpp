c149d74d7a0d80cf11820d8cc70dc31b82dbaca3
OleAgterberg <oleagterberg@gmail.com>
2015-10-26 16:15:01 +0100

    add class Collision

---
+++ Gate2158/src/Collision.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ole on 10/26/15.
+//
+
+#include "Collision.h"
---
+++ Gate2158/src/Collision.h
@@ -0,0 +1,15 @@
+//
+// Created by Ole on 10/26/15.
+//
+
+#ifndef GATE2158_COLLISION_H
+#define GATE2158_COLLISION_H
+
+class Collision {
+public:
+
+private:
+
+};
+
+#endif
