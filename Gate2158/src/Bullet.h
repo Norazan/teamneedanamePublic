325b4ef8cdd48a20cd40b80bf0e22fae53de52b8
OleAgterberg <oleagterberg@gmail.com>
2015-11-06 17:29:10 +0100

    add doxygen comments to the Bullet and Camera classes

--- Gate2158/src/Bullet.h
+++ Gate2158/src/Bullet.h
@@ -1,3 +1,11 @@
+/**
+* @file Bullet.h
+* @author  
+* @version 1.0
+*
+* The Bullet class represents a bullet on the screen.
+*/
+
 #ifndef GATE2158_BULLET_H
 #define GATE2158_BULLET_H
 
@@ -8,6 +16,24 @@
 class Camera;
 class Bullet : public MapObject {
 public:
+	/**
+	* Constructor that sets the variable of the bullet.
+	*
+	* @param bulletDamage 
+	*		The damage of the bullet
+	* @param startingLocation 
+	*		The location on the map
+	* @param startingVelocity 
+	*		The velocity of the bullet
+	* @param angle 
+	*		The angle of the bullet in degrees
+	* @param weapon 
+	*		The weapon that shoots the bullet
+	* @param isFriend
+	*		A number that shows which character has shoot, so it don't collide 	
+	* @param gunTexture
+	*		The texture of the bullet
+	*/
 	Bullet(
 		int bulletDamage,
 		sf::Vector2f startingLocation,
@@ -17,18 +43,67 @@ public:
 		int isFriend,
 		sf::Texture & gunTexture
 	);
+	/**
+	* Draws the bullet on the screen
+	*
+	* @param window
+	*		The window where the bullet will draw
+	* @param drawPosition
+	*		The position on the screen where the bullet will draw
+	*/
 	void draw(sf::RenderWindow & window, sf::Vector2f drawPosition) override;
+	/**
+	* Checked of the bullet out of bounds is
+	* 
+	* @param window
+	*		The window where the check takes place
+	* @return 
+	*		Boolean if bullet is out of bounds
+	*/
 	bool outOfBound(sf::RenderWindow & window);
+	/**
+	* Destroyed the bullet, sets the damage on param and sets the expoints on the weapon
+	*
+	* @param mo
+	*		The MapObject where the bullet has collide with.
+	*/
 	void collisionDetected(MapObject &mo) override;
 private:
+	/**
+	* the shape of the bullet
+	*/
 	sf::CircleShape bullet;
+	/**
+	* The weapon that shoots the bullet, for giving expoints
+	*/
 	Weapon* weapon;
+	/**
+	* The damage of the bullet
+	*/
 	int damage;
+	/**
+	* The location on the map
+	*/
 	sf::Vector2f location;
+	/**
+	* The velocity of the bullet
+	*/
 	sf::Vector2f velocity;
+	/**
+	* The speed of the bullet
+	*/
 	float bSpeed = 10;
+	/**
+	* The angle of the bullet
+	*/
 	float angle = 0;
+	/**
+	* Boolean if bullet has collide
+	*/
 	bool hasCollision = false;
+	/** 
+	* The Camera to remove the bullet of the screen
+	*/
 	Camera* c;
 };
 
--- Gate2158/src/Camera.hpp
+++ Gate2158/src/Camera.hpp
@@ -1,6 +1,12 @@
-//
-// Created by martijn on 9/29/15.
-//
+/**
+* @file Camera.hpp
+* @author Martijn
+* @date 9/29/15
+* @version 1.0
+*
+* The Camera class draws everything on the screen
+* THE CAMERA CLASS IS A SINGLETON CLASS, so there can only be one camera.
+*/
 
 #ifndef GATE2158_CAMERA_H
 #define GATE2158_CAMERA_H
@@ -14,38 +20,97 @@ class Map;
 
 class Camera {
 public:	
-
-	static Camera* getInstance();
-
-	//! Draw function
-	//
-	//! Drawed all the MapObjects 
+	/**
+	* Gives the Camera class
+	*
+	* @return
+	*		The only Camera class
+	*/
+	static Camera* getInstance(); 
+	/**
+	* Drawed all objects around the player
+	* And the function act() of the MapObjects will be called
+	*/
 	void draw();
+	/**
+	* Add a MapObject to the current Map
+	*
+	* @param mo
+	*		MapObject pointer that will be added to the current Map
+	*/
 	void addMapObjectToCurrentMap(MapObject *mo);
+	/**
+	* Removed a MapObject of the current Map
+	*
+	* @param mo
+	*		MapObject pointer that will be removed of the current Map
+	*/
 	void revmoveMapObjectOnCurrentMap(MapObject *mo);
-
+	/**
+	* Gives all objects on the current map that are around the player
+	*
+	* @return
+	*		A vector pointer of MapObject pointers
+	*/
 	std::vector<MapObject*> *getObjectsAroundPlayer();
-	
+	/**
+	* Checked for all object around the player if they collide
+	* If they collide, the collisonDetected funtion of the MapObjects will be called
+	*/
 	void checkCollision();
-
+	/**
+	* Sets the attribute window
+	*
+	* @param window
+	*		The window where the camera will draw
+	*/
 	void setWindow(sf::RenderWindow &window);
+	/**
+	* Sets the attribute currentMap
+	*
+	* @param currenMap
+	*		The map that will be drawed
+	*/
 	void setCurrentMap(Map & currentMap);
+	/**
+	* Sets the attribute currentPlayer
+	*
+	* @param currentPlayer
+	*		The current player
+	*/
 	void setCurrentPlayer(MapObject *currentPlayer);
-
+	/** 
+	* The window where the camera will draw 
+	*/
 	sf::RenderWindow *window;
 private:
-	//! Constructor of Camera
-	//
-	//! @parameter window
-	//! @parameter currenMap
-	//Camera(sf::RenderWindow &window, Map &currentMap);
-
+	/**
+	* Constructor of Camera
+	*/
 	Camera();
+	/**
+	* The camera class
+	*/
 	static Camera *camera;
+	/**
+	* The map that will be drawed
+	*/
 	Map *currentMap;
+	/**
+	* The current player to sets the draw positions
+	*/
 	MapObject *currentPlayer;
+	/**
+	* A list of MapObjects that will be removed of the map afther the draw.
+	*/
 	std::vector<MapObject*> removeObjects;
+	/**
+	* A list of MapObjects that will be added to the map afther the draw.
+	*/
 	std::vector<MapObject*> addObjects;
+	/**
+	* A list of MapObjects that are around the player.
+	*/
 	std::vector<MapObject*> *objectsAroundPlayer;
 };
 
